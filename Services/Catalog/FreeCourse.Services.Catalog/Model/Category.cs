7fa41fb81dc563c5293295a3b9886b1d55bd6354
kenanqedirov <k.qedirov1999@gmail.com>
2023-12-26 03:05:26 +0400

    Add project files.

---
+++ Services/Catalog/FreeCourse.Services.Catalog/Model/Category.cs
@@ -0,0 +1,13 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace FreeCourse.Services.Catalog.Model
+{
+    public class Category
+    {
+        [BsonId]     // Key olmağın göstərir
+        [BsonRepresentation(BsonType.ObjectId)]     // ObjectId tipine cevirir
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+}
