947c103cf4a567647ce8caf5d2ed849bb2e95fbe
kenanqedirov <k.qedirov1999@gmail.com>
2023-12-27 18:06:55 +0400

    Added Catalog Api (Microservice) and IdentityServer4

---
+++ Services/Catalog/FreeCourse.Services.Catalog/Services/CourseService.cs
@@ -0,0 +1,108 @@
+﻿using AutoMapper;
+using FreeCourse.Services.Catalog.Dtos;
+using FreeCourse.Services.Catalog.Model;
+using FreeCourse.Services.Catalog.Settings;
+using FreeCourse.Shared.Dtos;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Services.Catalog.Services
+{
+    public class CourseService : ICourseService
+    {
+        private readonly IMapper _mapper;
+        public readonly IMongoCollection<Course> _courseCollection;
+        public readonly IMongoCollection<Category> _categoryCollection;
+
+        public CourseService(IMapper mapper, IDatabaseSettings databaseSettings)
+        {
+            _mapper = mapper;
+            var client = new MongoClient(databaseSettings.ConnectionString);
+            var database = client.GetDatabase(databaseSettings.DatabaseName);
+
+            _courseCollection = database.GetCollection<Course>(databaseSettings.CourseCollectionName);
+            _categoryCollection = database.GetCollection<Category>(databaseSettings.CategoryCollectionName);
+        }
+        public async Task<Response<List<CourseDto>>> GetAllAsync()
+        {
+            var courses = await _courseCollection.Find(course => true).ToListAsync();
+
+            if (courses.Any())
+            {
+                foreach (var course in courses)
+                {
+                    course.Category = await _categoryCollection.Find<Category>(x => x.Id == course.CategoryId).FirstAsync();
+                }
+            }
+            else
+            {
+                courses = new List<Course>();
+            }
+            return Response<List<CourseDto>>.Success(_mapper.Map<List<CourseDto>>(courses), 200);
+        }
+
+        public async Task<Response<CourseDto>> GetByIdAsync(string id)
+        {
+            var course = await _courseCollection.Find<Course>(x => x.Id == id).FirstOrDefaultAsync();
+            if (course == null)
+            {
+                return Response<CourseDto>.Fail("Course not found", 404);
+            }
+            course.Category =await _categoryCollection.Find<Category>(a=>a.Id == course.CategoryId).FirstAsync();
+            
+            return Response<CourseDto>.Success(_mapper.Map<CourseDto>(course), 200);
+        }
+
+        public async Task<Response<List<CourseDto>>> GetAllByUserIdAsync(string userId)
+        {
+            var courses = await _courseCollection.Find<Course>(x=>x.UserId == userId).ToListAsync();
+            if (!courses.Any())
+            {
+                foreach (var course in courses)
+                {
+                    course.Category =await _categoryCollection.Find<Category>(a=>a.Id == course.CategoryId).FirstOrDefaultAsync();
+                }
+            }
+            else
+            {
+                courses = new List<Course>();
+            }
+            return Response<List<CourseDto>>.Success(_mapper.Map<List<CourseDto>>(courses), 200);
+        }
+
+        public async Task<Response<CourseDto>> CreateAsync (CourseCreateDto courseCreateDto)
+        {
+            var newCourse = _mapper.Map<Course>(courseCreateDto);
+            newCourse.CreatedTime = DateTime.Now;
+            await _courseCollection.InsertOneAsync(newCourse);
+            return Response<CourseDto>.Success(_mapper.Map<CourseDto>(newCourse), 200);
+        }
+
+        public async Task<Response<NoContent>> UpdateAsync(CourseUpdateDto courseUpdateDto)
+        {
+            var updateCourse = _mapper.Map<Course>(courseUpdateDto);
+            var result = await _courseCollection.FindOneAndReplaceAsync(a => a.Id == courseUpdateDto.Id, updateCourse);
+            if (result == null)
+            {
+                return Response<NoContent>.Fail("Course not found", 404);
+            }
+            return Response<NoContent>.Success(200);
+        } 
+
+        public async Task<Response<NoContent>> DeleteAsync(string id)
+        {
+            var result = await _courseCollection.DeleteOneAsync(a=>a.Id == id);
+            if (result.DeletedCount > 0)
+            {
+                return Response<NoContent>.Success(200);
+            }
+            else
+            {
+                return Response<NoContent>.Fail("Course not found",404);
+            }
+        }
+    }
+}
--- Shared/FreeCourse.Shared/Dtos/Response.cs
+++ Shared/FreeCourse.Shared/Dtos/Response.cs
@@ -1,11 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
 using System.Text.Json.Serialization;
 
 namespace FreeCourse.Shared.Dtos
 {
-    public class ResponseDto<T>
+    public class Response<T>
     {
         public T Data { get;private set; }
         [JsonIgnore]
@@ -15,26 +13,26 @@ namespace FreeCourse.Shared.Dtos
         public List<string> Errors { get; set; }    
 
 
-        public static ResponseDto<T> Success(T data, int statusCode)
+        public static Response<T> Success(T data, int statusCode)
         {
-            return new ResponseDto<T> { Data = data, StatusCode = statusCode ,isSuccessful=true };
+            return new Response<T> { Data = data, StatusCode = statusCode ,isSuccessful=true };
         }
-        public static ResponseDto<T> Success(int statusCode)
+        public static Response<T> Success(int statusCode)
         {
-            return new ResponseDto<T>() { Data = default(T), StatusCode = statusCode ,isSuccessful=true};
+            return new Response<T>() { Data = default(T), StatusCode = statusCode ,isSuccessful=true};
         }
-        public static ResponseDto<T> Fail (List<string> errors , int statusCode)
+        public static Response<T> Fail (List<string> errors , int statusCode)
         {
-            return new ResponseDto<T>()
+            return new Response<T>()
             {
                 Errors = errors ,
                 StatusCode = statusCode ,
                 isSuccessful=false
             };
         }
-        public static ResponseDto<T> Fail(string error , int statusCode)
+        public static Response<T> Fail(string error , int statusCode)
         {
-            return new ResponseDto<T>
+            return new Response<T>
             {
                Errors = new List<string> { error },StatusCode = statusCode ,isSuccessful=false
             };
