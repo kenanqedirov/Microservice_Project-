2ee6e33abefcf2623926c4420eceef3d2420ced3
kenanqedirov <k.qedirov1999@gmail.com>
2024-01-02 20:36:25 +0400

    Added Order Microservice

---
+++ Services/Order/FreeCourse.Services.Order/Controllers/OrdersController.cs
@@ -0,0 +1,38 @@
+﻿using FreeCourse.Services.Order.Application.Commands;
+using FreeCourse.Services.Order.Application.Queries;
+using FreeCourse.Shared.ControllerBases;
+using FreeCourse.Shared.Services;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Services.Order.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersController : CustomBaseController
+    {
+        private readonly IMediator _mediator;
+        private readonly ISharedIdentityService _sharedIdentityService;
+
+        public OrdersController(ISharedIdentityService sharedIdentityService, IMediator mediator)
+        {
+            _sharedIdentityService = sharedIdentityService;
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetOrders()
+        {
+            var response = await _mediator.Send(new GetOrdersByUserIdQuery { UserId = _sharedIdentityService.GetUserId }) ;
+            return CreateActionResultInstance(response);
+        }
+        [HttpPost]
+        public async Task<IActionResult> SaveOrder(CreateOrderCommand createOrderCommand)
+        {
+            var resource = await _mediator.Send(createOrderCommand);
+            return CreateActionResultInstance(resource);
+        }
+    }
+}
